83f810af636f1a39c4b92dc3f08f922e322f30eb
Sebastian Breit <sebastian.breitan+github@gmail.com>
2025-01-02 20:25:34 +0100

    add Deno LSP

--- init.lua
+++ init.lua
@@ -156,6 +156,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Custom Options
+require 'options'
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -622,6 +625,7 @@ require('lazy').setup({
         rust_analyzer = {},
         astro = {},
         bashls = {},
+        denols = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
