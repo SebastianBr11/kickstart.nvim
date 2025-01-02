42a1598aa8e5426d0d451a72a8f59f2c80455dec
Sebastian Breit <sebastian.breitan+github@gmail.com>
2025-01-02 19:07:27 +0100

    add Astro LSP

--- init.lua
+++ init.lua
@@ -620,6 +620,7 @@ require('lazy').setup({
         -- gopls = {},
         pyright = {},
         rust_analyzer = {},
+        astro = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
