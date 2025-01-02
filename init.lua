688a6dc58ee04c28f7c2ebce93f3f52ba4d7dea4
Sebastian Breit <sebastian.breitan+github@gmail.com>
2025-01-02 19:08:07 +0100

    add bash LSP, formatter and linter

--- init.lua
+++ init.lua
@@ -621,6 +621,7 @@ require('lazy').setup({
         pyright = {},
         rust_analyzer = {},
         astro = {},
+        bashls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -658,6 +659,8 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'shellcheck',
+        'shfmt',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
