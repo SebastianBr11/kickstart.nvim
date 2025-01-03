a6af5b92af1f6ccd76da7e8877b36d3dfb13c799
Sebastian Breit <sebastian.breitan+github@gmail.com>
2025-01-03 12:31:22 +0100

    add TailwindCSS LSP

--- init.lua
+++ init.lua
@@ -683,6 +683,7 @@ require('lazy').setup({
         bashls = {},
         denols = {},
         svelte = {},
+        tailwindcss = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
