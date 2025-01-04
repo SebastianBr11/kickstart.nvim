e0576f834a49d2ec0c30d30a32b21ae9b47aad2b
Sebastian Breit <sebastian.breitan+github@gmail.com>
2025-01-04 18:52:35 +0100

    correctly setup nvim-ts-autotag

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,8 +5,18 @@
 return {
   {
     'windwp/nvim-ts-autotag',
+    event = { 'BufReadPre', 'BufNewFile' },
     config = function()
-      require('nvim-ts-autotag').setup {}
+      require('nvim-ts-autotag').setup {
+        -- Also override individual filetype configs, these take priority.
+        -- Empty by default, useful if one of the "opts" global settings
+        -- doesn't work well in a specific filetype
+        per_filetype = {
+          ['html'] = {
+            enable_close = false,
+          },
+        },
+      }
     end,
   },
   {
