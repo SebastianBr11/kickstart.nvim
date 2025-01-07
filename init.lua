be517b2e2fa9ebaf4b9803e18f2a6ba93fedbfc8
Sebastian Breit <sebastian.breitan+github@gmail.com>
2025-01-07 19:08:42 +0100

    remove line numbers from terminal

--- init.lua
+++ init.lua
@@ -206,6 +206,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+require 'custom.terminal'
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
