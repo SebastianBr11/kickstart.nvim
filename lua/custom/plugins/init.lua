b78b373cd61ac944b7995806b5a09afae8360753
Sebastian Breit <sebastian.breitan+github@gmail.com>
2025-01-18 11:50:38 +0100

    move typescript plugins into own file

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,27 +4,6 @@
 -- See the kickstart.nvim README for more information
 return {
   {
-    'windwp/nvim-ts-autotag',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      require('nvim-ts-autotag').setup {
-        -- Also override individual filetype configs, these take priority.
-        -- Empty by default, useful if one of the "opts" global settings
-        -- doesn't work well in a specific filetype
-        per_filetype = {
-          ['html'] = {
-            enable_close = false,
-          },
-        },
-      }
-    end,
-  },
-  {
-    'pmizio/typescript-tools.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-    opts = {},
-  },
-  {
     'xiyaowong/transparent.nvim',
   },
 }
