ae71c9999134f960cce248f681c81da460d2aa9a
Sebastian Breit <sebastian.breitan+github@gmail.com>
2025-05-08 08:26:08 +0200

    add html and angular lsp

--- init.lua
+++ init.lua
@@ -631,8 +631,10 @@ require('lazy').setup({
         denols = {
           root_dir = require('lspconfig').util.root_pattern('deno.json', 'deno.jsonc'),
         },
+        html = {},
         svelte = {},
         tailwindcss = {},
+        angularls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         ts_ls = {},
@@ -735,6 +737,7 @@ require('lazy').setup({
         typescript = { 'prettierd' },
         javascriptreact = { 'prettierd' },
         typescriptreact = { 'prettierd' },
+        html = { 'prettierd' },
         css = { 'prettierd' },
         json = { 'prettierd' },
         svelte = { 'prettierd' },
