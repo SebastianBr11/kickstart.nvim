a93a01b8d1256052181a3e7629a69e91b578b6a5
Sebastian Breit <s.breit@snapaddy.com>
2025-05-30 12:56:59 +0200

    add smear-cursor.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,4 +6,15 @@ return {
   {
     'xiyaowong/transparent.nvim',
   },
+  {
+    'sphamba/smear-cursor.nvim',
+    -- Options for faster smear from the Github Page (https://github.com/sphamba/smear-cursor.nvim)
+    opts = { -- Default  Range
+      stiffness = 0.8, -- 0.6      [0, 1]
+      trailing_stiffness = 0.5, -- 0.4      [0, 1]
+      stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
+      trailing_stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
+      distance_stop_animating = 0.5, -- 0.1      > 0
+    },
+  },
 }
