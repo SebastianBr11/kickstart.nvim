1998d22f982a5b40533509c3d4ff39b22f155614
Sebastian Breit <s.breit@snapaddy.com>
2025-06-10 14:23:27 +0200

    replace pyright with basedpyright

--- init.lua
+++ init.lua
@@ -678,13 +678,11 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         -- gopls = {},
-        pyright = {
+        basedpyright = {
           settings = {
-            pyright = {
+            basedpyright = {
               -- Using Ruff's import organizer
               disableOrganizeImports = true,
-            },
-            python = {
               analysis = {
                 -- Ignore all files for analysis to exclusively use Ruff for linting
                 ignore = { '*' },
