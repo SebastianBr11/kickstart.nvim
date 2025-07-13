a631c189a53cd3e28d1dc6e8f2885b606a8e8b5c
Sebastian Breit <sebastian.breitan+github@gmail.com>
2025-07-13 14:06:53 +0200

    remove nvim-spider

--- lua/custom/plugins/movement.lua
+++ lua/custom/plugins/movement.lua
@@ -1,13 +1,5 @@
 return {
   {
-    'chrisgrieser/nvim-spider',
-    keys = {
-      { 'w', "<cmd>lua require('spider').motion('w')<CR>", mode = { 'n', 'o', 'x' } },
-      { 'e', "<cmd>lua require('spider').motion('e')<CR>", mode = { 'n', 'o', 'x' } },
-      { 'b', "<cmd>lua require('spider').motion('b')<CR>", mode = { 'n', 'o', 'x' } },
-    },
-  },
-  {
     'folke/flash.nvim',
     event = 'VeryLazy',
     ---@type Flash.Config
