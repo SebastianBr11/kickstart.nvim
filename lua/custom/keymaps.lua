b753a22eaa4ec415321d21460c3294d94cff2949
Sebastian Breit <s.breit@snapaddy.com>
2025-07-23 15:15:35 +0200

    improve movement in long soft-wrapped lines

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,5 +1,28 @@
+local set = vim.keymap.set
+
 for i = 1, 9 do
-  vim.keymap.set({ 'i', 'n' }, '<C-' .. i .. '>', i .. 'gt', { unique })
+  set({ 'i', 'n' }, '<C-' .. i .. '>', i .. 'gt', { unique })
 end
 
-vim.keymap.set({ 'i', 'n' }, '<C-0>', ':tablast<CR>')
+set({ 'i', 'n' }, '<C-0>', ':tablast<CR>')
+
+-- Thanks TJ
+set('n', 'j', function(...)
+  local count = vim.v.count
+
+  if count == 0 then
+    return 'gj'
+  else
+    return 'j'
+  end
+end, { expr = true })
+
+set('n', 'k', function(...)
+  local count = vim.v.count
+
+  if count == 0 then
+    return 'gk'
+  else
+    return 'k'
+  end
+end, { expr = true })
