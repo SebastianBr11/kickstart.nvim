9cc6ca743c6416beb636aa853b25d2a7a17f1444
Sebastian Breit <s.breit@snapaddy.com>
2025-07-24 13:20:54 +0200

    disable tiny-inline-diagnostic.nvim

--- lua/custom/plugins/diagnostics.lua
+++ lua/custom/plugins/diagnostics.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    'rachartier/tiny-inline-diagnostic.nvim',
-    event = 'VeryLazy', -- Or `LspAttach`
-    priority = 1000, -- needs to be loaded in first
-    config = function()
-      require('tiny-inline-diagnostic').setup()
-
-      -- Disable default diagnostics
-      vim.diagnostic.config {
-        virtual_text = false,
-      }
-    end,
-  },
+  -- {
+  --   'rachartier/tiny-inline-diagnostic.nvim',
+  --   event = 'VeryLazy', -- Or `LspAttach`
+  --   priority = 1000, -- needs to be loaded in first
+  --   config = function()
+  --     require('tiny-inline-diagnostic').setup()
+  --
+  --     -- Disable default diagnostics
+  --     vim.diagnostic.config {
+  --       virtual_text = false,
+  --     }
+  --   end,
+  -- },
 }
