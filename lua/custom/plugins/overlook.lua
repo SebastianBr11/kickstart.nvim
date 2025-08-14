824b94f9c04915fe59ed2f5cefa5473d661f6beb
Sebastian Breit <s.breit@snapaddy.com>
2025-08-14 14:26:22 +0200

    add overlook.nvim

---
+++ lua/custom/plugins/overlook.lua
@@ -0,0 +1,81 @@
+return {
+  {
+    'williamhsieh/overlook.nvim',
+
+    ---@type OverlookOptions
+    opts = {},
+
+    keys = {
+      {
+        '<leader>po',
+        function()
+          require('overlook.api').peek_cursor()
+        end,
+        desc = 'Overlook peek cursor',
+      },
+      {
+        '<leader>pd',
+        function()
+          require('overlook.api').peek_definition()
+        end,
+        desc = 'Overlook peek definition',
+      },
+      {
+        '<leader>pc',
+        function()
+          require('overlook.api').close_all()
+        end,
+        desc = 'Overlook close all popups',
+      },
+      {
+        '<leader>pf',
+        function()
+          require('overlook.api').switch_focus()
+        end,
+        desc = 'Overlook switch focus',
+      },
+      {
+        '<leader>pv',
+        function()
+          require('overlook.api').open_in_vsplit()
+        end,
+        desc = 'Overlook open in vsplit',
+      },
+      {
+        '<leader>ps',
+        function()
+          require('overlook.api').open_in_split()
+        end,
+        desc = 'Overlook open in split',
+      },
+      {
+        '<leader>pt',
+        function()
+          require('overlook.api').open_in_tab()
+        end,
+        desc = 'Overlook open in tab',
+      },
+      {
+        '<leader>pq',
+        function()
+          require('overlook.api').open_in_original_window()
+        end,
+        desc = 'Overlook open in original window',
+      },
+      {
+        '<leader>pu',
+        function()
+          require('overlook.api').restore_popup()
+        end,
+        desc = 'Overlook restore popup',
+      },
+      {
+        '<leader>pU',
+        function()
+          require('overlook.api').restore_all_popups()
+        end,
+        desc = 'Overlook restore all popups',
+      },
+    },
+  },
+}
