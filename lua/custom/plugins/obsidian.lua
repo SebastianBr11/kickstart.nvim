07531decfb66eb0cd9b792ce2f94d0ab2af2de39
Sebastian Breit <s.breit@snapaddy.com>
2025-08-14 13:51:52 +0200

    update obsidian.nvim config

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -3,40 +3,29 @@ return {
     'obsidian-nvim/obsidian.nvim',
     version = '*', -- recommended, use latest release instead of latest commit
     lazy = true,
-    ft = 'markdown',
     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-    --   -- refer to `:h file-pattern` for more examples
-    --   "BufReadPre path/to/my-vault/*.md",
-    --   "BufNewFile path/to/my-vault/*.md",
-    -- },
-    dependencies = {
-      -- Required.
-      'nvim-lua/plenary.nvim',
-
-      -- Optional dependencies
-      'saghen/blink.cmp',
-      'nvim-telescope/telescope.nvim',
-      'nvim-treesitter/nvim-treesitter',
+    event = {
+      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+      -- refer to `:h file-pattern` for more examples
+      'BufReadPre '
+        .. vim.fn.expand '~'
+        .. '/SynologyDrive/Obsidian/Persönlich',
+      'BufNewFile ' .. vim.fn.expand '~' .. '/SynologyDrive/Obsidian/Persönlich',
+      'BufReadPre ' .. vim.fn.expand '~' .. '/SynologyDrive/Obsidian/Schule',
+      'BufNewFile ' .. vim.fn.expand '~' .. '/SynologyDrive/Obsidian/Schule',
     },
-    config = function()
-      require('obsidian').setup {
-        workspaces = {
-          {
-            name = 'Persönlich',
-            path = '~/SynologyDrive/Obsidian/Persönlich',
-          },
-          {
-            name = 'Schule',
-            path = '~/SynologyDrive/Obsidian/Schule',
-          },
+    opts = {
+      workspaces = {
+        {
+          name = 'Persönlich',
+          path = '~/SynologyDrive/Obsidian/Persönlich',
         },
-      }
-      -- Some syntax features require this to be set
-      -- This issue offers more details: https://github.com/epwalsh/obsidian.nvim/issues/286
-      vim.opt_local.conceallevel = 2
-    end,
+        {
+          name = 'Schule',
+          path = '~/SynologyDrive/Obsidian/Schule',
+        },
+      },
+    },
   },
 }
