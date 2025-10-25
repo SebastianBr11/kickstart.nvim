77e35f7d5e240de421174375a47f17789984e6b8
Sebastian Breit <sebastian.breitan+github@gmail.com>
2025-10-25 17:00:24 +0200

    add yamlls and use schemastore.nvim for it

--- init.lua
+++ init.lua
@@ -819,6 +819,20 @@ require('lazy').setup({
             },
           },
           vue_ls = {},
+          yamlls = {
+            settings = {
+              yaml = {
+                schemaStore = {
+                  -- You must disable built-in schemaStore support if you want to use
+                  -- this plugin and its advanced options like `ignore`.
+                  enable = false,
+                  -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                  url = '',
+                },
+                schemas = require('schemastore').yaml.schemas(),
+              },
+            },
+          },
         },
         -- This table contains config for all language servers that are *not* installed via Mason.
         -- Structure is identical to the mason table from above.
