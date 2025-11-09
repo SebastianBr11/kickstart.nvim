a7cc1b08179d32d91a8ad954d14af210468de3db
Sebastian Breit <sebastian.breitan+github@gmail.com>
2025-11-09 15:57:57 +0100

    add overseer.nvim

---
+++ lua/custom/plugins/overseer.lua
@@ -0,0 +1,8 @@
+return {
+  'stevearc/overseer.nvim',
+  keys = {
+    { '<leader>ot', '<cmd>OverseerToggle<CR>', desc = '[O]verseer [T]oggle' },
+    { '<leader>or', '<cmd>OverseerRun<CR>', desc = '[O]verseer [R]un' },
+  },
+  opts = {},
+}
