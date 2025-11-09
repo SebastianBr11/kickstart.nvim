00cfa17f29a45647e82135cb36ee9da4fb123c26
Sebastian Breit <sebastian.breitan+github@gmail.com>
2025-11-09 14:44:14 +0100

    add typescript-svelte-plugin to vtsls

--- init.lua
+++ init.lua
@@ -854,13 +854,6 @@ require('lazy').setup({
                 tsserver = {
                   globalPlugins = {
                     {
-                      name = '@vue/typescript-plugin',
-                      location = vim.fn.stdpath 'data' .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
-                      languages = { 'vue' },
-                      configNamespace = 'typescript',
-                      enableForWorkspaceTypeScriptVersions = true,
-                    },
-                    {
                       name = '@angular/language-server',
                       location = vim.fn.stdpath 'data' .. '/mason/packages/angular-language-server/node_modules/@angular/language-server',
                       -- Don't use typescript workspace version as recommended by Angular
@@ -870,6 +863,18 @@ require('lazy').setup({
                     {
                       name = '@astrojs/ts-plugin',
                       location = vim.fn.stdpath 'data' .. '/mason/packages/astro-language-server/node_modules/@astrojs/ts-plugin',
+                      enableForWorkspaceTypeScriptVersions = true,
+                    },
+                    {
+                      name = '@vue/typescript-plugin',
+                      location = vim.fn.stdpath 'data' .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
+                      languages = { 'vue' },
+                      configNamespace = 'typescript',
+                      enableForWorkspaceTypeScriptVersions = true,
+                    },
+                    {
+                      name = 'typescript-svelte-plugin',
+                      location = vim.fn.stdpath 'data' .. '/mason/packages/svelte-language-server/node_modules/typescript-svelte-plugin',
                       enableForWorkspaceTypeScriptVersions = true,
                     },
                   },
