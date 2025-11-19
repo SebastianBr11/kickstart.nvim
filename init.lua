756898d067b6713b503bc9e36563470f532b45d5
Sebastian Breit <s.breit@snapaddy.com>
2025-11-19 14:08:17 +0100

    disable blink.cmp signature window
    I'm using the one from noice

--- init.lua
+++ init.lua
@@ -1199,7 +1199,7 @@ require('lazy').setup({
       fuzzy = { implementation = 'prefer_rust_with_warning' },
 
       -- Shows a signature help window while you type arguments for a function
-      signature = { enabled = true },
+      signature = { enabled = false },
     },
   },
 
