773224c386577b07bd62f07397fbb776863c7d23
Sebastian Breit <s.breit@snapaddy.com>
2025-11-19 10:55:36 +0100

    use minimal lensline config

--- lua/custom/plugins/lens.lua
+++ lua/custom/plugins/lens.lua
@@ -5,6 +5,17 @@ return {
     'oribarilan/lensline.nvim',
     branch = 'release/2.x', -- or: branch = 'release/2.x' for latest non-breaking updates
     event = 'LspAttach',
-    opts = {},
+    opts = {
+      profiles = {
+        {
+          name = 'minimal',
+          style = {
+            placement = 'inline',
+            prefix = '',
+            render = 'focused',
+          },
+        },
+      },
+    },
   },
 }
