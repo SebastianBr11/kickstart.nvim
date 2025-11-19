21b8b3e185324f91710e7a3a3d385ec5662e9776
Sebastian Breit <s.breit@snapaddy.com>
2025-11-19 10:45:19 +0100

    add lensline.nvim

---
+++ lua/custom/plugins/lens.lua
@@ -0,0 +1,10 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+  {
+    'oribarilan/lensline.nvim',
+    branch = 'release/2.x', -- or: branch = 'release/2.x' for latest non-breaking updates
+    event = 'LspAttach',
+    opts = {},
+  },
+}
