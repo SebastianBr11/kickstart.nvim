95faa884bc85332ad7b3fc7363bd7ec217cdf27c
Sebastian Breit <s.breit@snapaddy.com>
2025-11-19 14:19:10 +0100

    add borders to blink.cmp completion menu

--- init.lua
+++ init.lua
@@ -1123,9 +1123,10 @@ require('lazy').setup({
       completion = {
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
-        documentation = { auto_show = false, auto_show_delay_ms = 500 },
+        documentation = { auto_show = true, window = { border = 'rounded' } },
         -- To integrate blink.cmp with `nvim-highlight-colors`
         menu = {
+          border = 'rounded',
           draw = {
             components = {
               -- customize the drawing of kind icons
