c45a2b6bbc038d1034fdb9b662d0bd719c3abf97
Sebastian Breit <sebastian.breitan+github@gmail.com>
2026-01-31 13:41:53 +0100

    Update obsidian.nvim config

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -2,28 +2,19 @@ return {
   {
     'obsidian-nvim/obsidian.nvim',
     version = '*', -- recommended, use latest release instead of latest commit
-    lazy = true,
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    event = {
-      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-      -- refer to `:h file-pattern` for more examples
-      'BufReadPre '
-        .. vim.fn.expand '~'
-        .. '/SynologyDrive/Obsidian/Persönlich',
-      'BufNewFile ' .. vim.fn.expand '~' .. '/SynologyDrive/Obsidian/Persönlich',
-      'BufReadPre ' .. vim.fn.expand '~' .. '/SynologyDrive/Obsidian/Schule',
-      'BufNewFile ' .. vim.fn.expand '~' .. '/SynologyDrive/Obsidian/Schule',
-    },
+    ft = 'markdown',
+    ---@module 'obsidian'
+    ---@type obsidian.config
     opts = {
+      legacy_commands = false, -- this will be removed in the next major release
       workspaces = {
-        {
-          name = 'Persönlich',
-          path = '~/SynologyDrive/Obsidian/Persönlich',
-        },
         {
           name = 'Schule',
           path = '~/SynologyDrive/Obsidian/Schule',
+        },
+        {
+          name = 'My Vault',
+          path = '~/SynologyDrive/Obsidian/sebastian-vault',
         },
       },
     },
