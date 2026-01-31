42a16aa24bdd49e54affb63c5cd63bd557d83422
Sebastian Breit <sebastian.breitan+github@gmail.com>
2026-01-31 15:37:46 +0100

    Add neotest and the vitest adapter

---
+++ lua/custom/plugins/neotest.lua
@@ -0,0 +1,56 @@
+return {
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'marilari88/neotest-vitest',
+      'stevearc/overseer.nvim',
+    },
+    opts = function()
+      return {
+        consumers = {
+          overseer = require 'neotest.consumers.overseer',
+        },
+        adapters = {
+          require 'neotest-vitest',
+        },
+      }
+    end,
+    keys = {
+      {
+        '<leader>Tr',
+        function() require('neotest').run.run() end,
+        desc = 'Run',
+      },
+      {
+        '<leader>Ts',
+        function() require('neotest').run.stop() end,
+        desc = 'Stop',
+      },
+      {
+        '<leader>Ta',
+        function() require('neotest').run.attach() end,
+        desc = 'Attach',
+      },
+
+      {
+        '<leader>Tm',
+        function() require('neotest').summary.toggle() end,
+        desc = 'Summary',
+      },
+      {
+        '<leader>Tw',
+        function() require('neotest').watch.toggle() end,
+        desc = 'Watch',
+      },
+      {
+        '<leader>To',
+        function() require('neotest').output_panel.toggle() end,
+        desc = 'Output panel',
+      },
+    },
+  },
+}
