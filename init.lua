6170921a74936bc9a93f672fcee75ae0380b2232
Sebastian Breit <sebastian.breitan+github@gmail.com>
2026-03-08 18:21:27 +0100

    Format snacks picker keymaps into single lines

--- init.lua
+++ init.lua
@@ -450,74 +450,21 @@ require('lazy').setup({
 
     -- See `:help snacks-pickers-sources`
     keys = {
-      {
-        '<leader>sh',
-        function() Snacks.picker.help() end,
-        desc = '[S]earch [H]elp',
-      },
-      {
-        '<leader>sk',
-        function() Snacks.picker.keymaps() end,
-        desc = '[S]earch [K]eymaps',
-      },
-      {
-        '<leader>sf',
-        function() Snacks.picker.smart() end,
-        desc = '[S]earch [F]iles',
-      },
-      {
-        '<leader>ss',
-        function() Snacks.picker.pickers() end,
-        desc = '[S]earch [S]elect Snacks',
-      },
-      {
-        '<leader>sw',
-        function() Snacks.picker.grep_word() end,
-        desc = '[S]earch current [W]ord',
-        mode = { 'n', 'x' },
-      },
-      {
-        '<leader>sg',
-        function() Snacks.picker.grep() end,
-        desc = '[S]earch by [G]rep',
-      },
-      {
-        '<leader>sd',
-        function() Snacks.picker.diagnostics() end,
-        desc = '[S]earch [D]iagnostics',
-      },
-      {
-        '<leader>sr',
-        function() Snacks.picker.resume() end,
-        desc = '[S]earch [R]esume',
-      },
-      {
-        '<leader>s.',
-        function() Snacks.picker.recent() end,
-        desc = '[S]earch Recent Files ("." for repeat)',
-      },
-      {
-        '<leader><leader>',
-        function() Snacks.picker.buffers() end,
-        desc = '[ ] Find existing buffers',
-      },
-      {
-        '<leader>/',
-        function() Snacks.picker.lines {} end,
-        desc = '[/] Fuzzily search in current buffer',
-      },
+      { '<leader>sh', function() Snacks.picker.help() end, desc = '[S]earch [H]elp' },
+      { '<leader>sk', function() Snacks.picker.keymaps() end, desc = '[S]earch [K]eymaps' },
+      { '<leader>sf', function() Snacks.picker.smart() end, desc = '[S]earch [F]iles' },
+      { '<leader>ss', function() Snacks.picker.pickers() end, desc = '[S]earch [S]elect Snacks' },
+      { '<leader>sw', function() Snacks.picker.grep_word() end, desc = '[S]earch current [W]ord', mode = { 'n', 'x' } },
+      { '<leader>sg', function() Snacks.picker.grep() end, desc = '[S]earch by [G]rep' },
+      { '<leader>sd', function() Snacks.picker.diagnostics() end, desc = '[S]earch [D]iagnostics' },
+      { '<leader>sr', function() Snacks.picker.resume() end, desc = '[S]earch [R]esume' },
+      { '<leader>s.', function() Snacks.picker.recent() end, desc = '[S]earch Recent Files ("." for repeat)' },
+      { '<leader><leader>', function() Snacks.picker.buffers() end, desc = '[ ] Find existing buffers' },
+      { '<leader>/', function() Snacks.picker.lines {} end, desc = '[/] Fuzzily search in current buffer' },
       { '<leader>sb', function() Snacks.picker.lines() end, desc = '[S]earch [B]uffer Lines' },
-      {
-        '<leader>s/',
-        function() Snacks.picker.grep_buffers() end,
-        desc = '[S]earch [/] in Open Files',
-      },
+      { '<leader>s/', function() Snacks.picker.grep_buffers() end, desc = '[S]earch [/] in Open Files' },
       -- Shortcut for searching your Neovim configuration files
-      {
-        '<leader>sn',
-        function() Snacks.picker.files { cwd = vim.fn.stdpath 'config' } end,
-        desc = '[S]earch [N]eovim files',
-      },
+      { '<leader>sn', function() Snacks.picker.files { cwd = vim.fn.stdpath 'config' } end, desc = '[S]earch [N]eovim files' },
       { '<leader>gl', function() Snacks.picker.git_log { confirm = 'git_show' } end, desc = 'Git Log' },
       { '<leader>gb', function() Snacks.gitbrowse.open() end, desc = '[G]it [B]rowse' },
       { '<leader>gc', function() Snacks.picker.git_branches() end, desc = '[G]it [C]heckout' },
