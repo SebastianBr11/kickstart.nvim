5f343f1df7355142a75d2988a74d8a4d62d7ad65
Sebastian Breit <sebastian.breitan+github@gmail.com>
2026-03-08 18:17:20 +0100

    Add back lua_ls
    I don't know when this was deleted

--- init.lua
+++ init.lua
@@ -839,6 +839,17 @@ require('lazy').setup({
               },
             },
           },
+          lua_ls = {
+            settings = {
+              Lua = {
+                completion = {
+                  callSnippet = 'Replace',
+                },
+                -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+                -- diagnostics = { disable = { 'missing-fields' } },
+              },
+            },
+          },
           marksman = {},
           postgres_lsp = {},
           ruff = {},
