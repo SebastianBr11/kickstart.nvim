e37ed98b37a402d3b1203168931c3a9d04eb627e
Sebastian Breit <s.breit@snapaddy.com>
2026-04-02 13:27:29 +0200

    Remove ft from obsidian plugin

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -2,7 +2,6 @@ return {
   {
     'obsidian-nvim/obsidian.nvim',
     version = '*', -- recommended, use latest release instead of latest commit
-    ft = 'markdown',
     ---@module 'obsidian'
     ---@type obsidian.config
     opts = {
