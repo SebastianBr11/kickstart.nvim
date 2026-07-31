e9bc605c54cf06056b3596e2c402c047862726d9
Sebastian Breit <41546861+SebastianBr11@users.noreply.github.com>
2026-07-31 15:47:02 +0200

    Remove unused themes

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -1,46 +1,5 @@
 return {
   {
-    'serhez/teide.nvim',
-    lazy = false,
-    priority = 1000,
-    opts = { transparent = true },
-    -- init = function() vim.cmd.colorscheme 'teide' end,
-  },
-  {
-    'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    opts = {
-      transparent = true,
-      comments = {
-        italic = false,
-      },
-    },
-  },
-  {
-    'oskarnurm/koda.nvim',
-    lazy = false,
-    priority = 1000,
-    opts = {
-      transparent = true,
-      styles = {
-        comments = {
-          fg = '#dfdf8e',
-        },
-      },
-    },
-    -- init = function() vim.cmd.colorscheme 'koda' end,
-  },
-  {
-    'yonatan-perel/lake-dweller.nvim',
-    lazy = false,
-    priority = 1000,
-    opts = {
-      transparent = true,
-    },
-    -- init = function() vim.cmd.colorscheme 'lake-dweller' end,
-  },
-  {
     'rebelot/kanagawa.nvim',
     lazy = false,
     priority = 1000,
